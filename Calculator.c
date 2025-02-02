79ceb9ec58eea5f053f4c6c608c25ecf3ffa979c
crabbypattubaby <udaylaxman300@gmail.com>
2025-02-02 21:45:08 +0530

    The Caculator code is finally completed, with loop
    The code is completed with loops and all, but it is still open to to more complex add-ons, such that it would make it more efficient, etc.

--- Calculator.c
+++ Calculator.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <math.h>
+# include <stdbool.h>
 
 int main(){
-
     float num1, num2;
+    bool end = true;
 
-    printf("Hello GitHub, this is my first time using it!!");
-    printf("\nThis is a calculator app, plsease enther your Number:\n");
+    
+    printf("\nThis is a calculator application!!\n");
+    do {
+    printf("\nPlease enter you first number:\n");
     scanf("%f", &num1);
     printf("\nPlease enter you second number:\n");
     scanf("%f", &num2);
-    printf("\nNow, you need to tell me what you want to do with these numbers. Enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division, 5 for power, 6 for square root:\n");
+    printf("\nNow, you need to tell me what you want to do with these numbers. Enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division, 5 for power, 6 for square root, -1 to exit:\n");
     int choice;
     scanf("%d", &choice);
     switch (choice)
@@ -19,24 +22,29 @@ int main(){
         printf("The sum of the two numbers is: %f", num1 + num2);
         break;
     case 2:
-        printf("The difference of the two numbers is: %f", num1 - num2);
+        printf("The difference of the two numbers is: %.2f", num1 - num2);
         break;
     case 3:
-        printf("The product of the two numbers is: %f", num1 * num2);
+        printf("The product of the two numbers is: %.2f", num1 * num2);
         break;
     case 4:
-        printf("The division of the two numbers is: %f", num1 / num2);
+        printf("The division of the two numbers is: %.2f", num1 / num2);
         break;
     case 5:
-        printf("The power of the two numbers is: %f", pow(num1, num2));
+        printf("The power of the two numbers is: %.2f", pow(num1, num2));
         break;
     case 6:
-        printf("The square root of the first number is: %f", sqrt(num1));
+        printf("The square root of the first number is: %.2f", sqrt(num1));
+        break;
+    case -1:
+        end = false;
+        printf("Exiting the calculator application");
         break;
     default:
         printf("Invalid choice");
         break;
     };
+    } while (end);
     
 
 };
